d6a654450bc2757f6b0eb5fb494f6202e84137a5
Prince-Jil <princejil01@gmail.com>
2025-07-13 20:17:15 +0100

    UPDATED assignment 7

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 const expression =["C is fun", "Python is cool", "JavaScript is amazing"];
 let i=0;
-while(i < expression.length){
+while (i < expression.length){
 console.log(expression[i]);
 i++;
 }
