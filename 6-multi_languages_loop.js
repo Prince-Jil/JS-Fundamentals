f489da7ce77cef3f7dc2b6c3870585745d480544
Jil Nnorom <Princejil01@gmail.com>
2025-07-20 23:59:51 +0100

    Update 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,5 @@
-const lines =["C is fun", "Python is cool", "JavaScript is amazing"];
-for (let i = 0; i< lines.length; i++){
-    console.log(lines[i])
+const langs = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (const lang of langs) {
+  console.log(lang);
 }
