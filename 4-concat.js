819ae179bbca8f8adf62ded7ec1095bb56432e4e
Jil Nnorom <Princejil01@gmail.com>
2025-07-20 23:50:47 +0100

    Update 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -2,8 +2,6 @@ const args = process.argv.slice(2);
 
 if (args[0] === undefined || args[1] === undefined) {
   console.log("undefined is undefined");
-} else if (args[0] === True || args[1] === undefined) {
-  console.log("${args[0]} is undefined");
 } else {
   console.log(`${args[0]} is ${args[1]}`);
 }
